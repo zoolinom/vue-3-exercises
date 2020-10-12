9de22eb5a4a4e0e6e683359640e2551d9a1e08af
Zoran Damjanov <zoolinom@gmail.com>
2020-10-12 21:18:41 +0200

    Multiple v models

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,16 @@
   <!-- <Another /> -->
   <!-- <HelloWorld /> -->
   <!-- <ComponentWithModal /> -->
-  <Vue2DetectionCaveat />
+  <!-- <Vue2DetectionCaveat /> -->
+
+  <!-- Synchronizing v-model with component InviteForm -->
+  <!-- v-model:parameter where parameter is name of parameter, in this case it is name -->
+  <!-- Parameter is passed as prop to component -->
+  <InviteForm v-model:name="inviteName" v-model:email="inviteEmail" />
+  <div>
+    <div>{{ inviteName }}</div>
+    <div>{{ inviteEmail }}</div>
+  </div>
 </template>
 
 <script>
@@ -12,6 +21,8 @@ import Another from './components/Another.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import ComponentWithModal from './components/ComponentWithModal.vue'
 import Vue2DetectionCaveat from './components/Vue2DetectionCaveat.vue'
+import InviteForm from './components/InviteForm.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'App',
@@ -20,7 +31,17 @@ export default {
     Another,
     HelloWorld,
     ComponentWithModal,
-    Vue2DetectionCaveat
+    Vue2DetectionCaveat,
+    InviteForm
+  },
+  setup() {
+    const inviteName = ref('')
+    const inviteEmail = ref('')
+
+    return {
+      inviteName,
+      inviteEmail
+    }
   }
 }
 </script>
---
+++ src/components/InviteForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form action="">
+    <div>
+      <label for="name">Name</label>
+      <!-- Name of eemitted event is important, so we have to say update:prop (name) -->
+      <input 
+        type="text" 
+        class="border ml-2"
+        :value="name"
+        @input="event => $emit('update:name', event.target.value)"
+      >
+    </div>
+    <div>
+      <label for="email">Email</label>
+      <input 
+        type="text" 
+        class="border ml-2"
+        :value="email"
+        @input="event => $emit('update:email', event.target.value)"
+      >
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ['name', 'email']
+}
+</script>
